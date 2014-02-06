43385ce5befe6ef2f1430d7f2684fa4f66097a5d
caracheng <ccheng@macalester.edu>
2014-02-06 15:25:48 -0600

    Changes made to allow programs to run.

--- src/edu/macalester/comp124/life/Conway.java
+++ src/edu/macalester/comp124/life/Conway.java
@@ -18,7 +18,25 @@ public class Conway implements RuleSet {
      * @param neighborCount The number of living neighbors of the cell.
      * @return true if the cell should be alive in the next generation.
      */
-    public boolean applyRules(boolean isAlive, int neighborCount) {
+    public boolean applyRules(boolean isAlive,int neighborCount ) {
+        if(isAlive)
+        {
+            if(neighborCount == 2)
+                {return true;}
+            else if(neighborCount==3)
+                {return true;}
+            else if(neighborCount < 2 )
+                {return false;}
+            else if(neighborCount > 3)
+                {return false;}
+        }
+        else if(!isAlive)
+        {
+            if(neighborCount == 3)
+                return true;
+            else
+                return false;
+        }
         return true;
     }
 }
